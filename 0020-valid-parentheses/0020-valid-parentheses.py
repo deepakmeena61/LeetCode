0cc79e3b04d8582a80db5950ffffdd92d83003bf
Deepak Meena <meena.d@northeastern.edu>
2025-05-10 15:08:55 -0400

    Time: 0 ms (100%), Space: 18.1 MB (10.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,8 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        closetoopen = {')':'(', ']':'[', '}':'{'}
-
+        closetoopen = {'}':'{', ']':'[', ')':'('}
         for c in s:
             if c in closetoopen:
                 if stack and stack[-1] == closetoopen[c]:
@@ -11,5 +10,4 @@ class Solution:
                     return False
             else:
                 stack.append(c)
-        
         return True if not stack else False
