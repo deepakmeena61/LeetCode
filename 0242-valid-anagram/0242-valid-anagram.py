77cd20f1c210e47d03ea2daf55c791b0e7f4b592
Deepak Meena <meena.d@northeastern.edu>
2025-05-10 14:19:02 -0400

    Time: 15 ms (42.7%), Space: 18 MB (49.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,7 +3,6 @@ class Solution:
         if len(s) != len(t):
             return False
         countS, countT = {}, {}
-
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
